c90f490a3cb4019229731d68a65ca662aa06267a
norbert_blaszczyk_bigcheese <norbert.blaszczyk@bigcheesestudio.com>
2021-01-29 23:16:16 +0100

    Enemies spawning, auto player follow when player move

---
+++ Assets/Scripts/EnemiesSystem/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using ObjectPooling;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour, IRestorable
+{
+	[SerializeField]
+	private NavMeshAgent navMeshAgent;
+
+	private void Awake()
+	{
+		navMeshAgent.enabled = false;
+	}
+
+	private void OnEnable()
+	{
+		navMeshAgent.enabled = true;
+	}
+
+	public void Restore()
+	{
+		navMeshAgent.enabled = false;
+	}
+
+	public void FollowPlayer(Vector3 playerPosition)
+	{
+		navMeshAgent.SetDestination(playerPosition);
+	}
+}
