161bec8aca56496eabd8c8428f9c04b1b77f1bfa
Zielon <zielon94@gmail.com>
2021-01-29 17:38:11 +0100

    Added object pooling.

---
+++ Assets/Scripts/ObjectPooling/Editor/ObjectPoolEditor.cs
@@ -0,0 +1,47 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace ObjectPooling
+{
+
+    [CustomEditor(typeof(ObjectPool))]
+    public class ObjectPoolEditor : Editor
+    {
+        ObjectPool objectPool;
+        Color guiBackgroundColor;
+        float totalPoolUsage;
+        float currentPoolUsage;
+
+        private void OnEnable()
+        {
+            objectPool = (ObjectPool)target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            EditorGUILayout.Space();
+
+            guiBackgroundColor = GUI.backgroundColor;
+
+            currentPoolUsage = (objectPool.PoolSize > 0 ? (float)objectPool.UsedObjectCount / objectPool.PoolSize : 0);
+            if (currentPoolUsage > 0.5f) GUI.backgroundColor = Color.Lerp(Color.yellow, Color.red, (currentPoolUsage - 0.5f) * 2.0f);
+            else GUI.backgroundColor = Color.Lerp(guiBackgroundColor, Color.yellow, currentPoolUsage * 2.0f);
+            EditorGUI.ProgressBar(EditorGUILayout.GetControlRect(), currentPoolUsage, $"Current pool Usage: {Mathf.RoundToInt(currentPoolUsage * 100)}%");
+
+            totalPoolUsage = (objectPool.PoolSize > 0 ? (float)objectPool.UsedObjectCount / objectPool.MaxPoolSize : 0);
+            if (totalPoolUsage > 0.5f) GUI.backgroundColor = Color.Lerp(Color.yellow, Color.red, (totalPoolUsage - 0.5f) * 2.0f);
+            else GUI.backgroundColor = Color.Lerp(guiBackgroundColor, Color.yellow, totalPoolUsage * 2.0f);
+            EditorGUI.ProgressBar(EditorGUILayout.GetControlRect(), totalPoolUsage, $"Total pool Usage: {Mathf.RoundToInt(totalPoolUsage * 100)}%");
+
+            GUI.backgroundColor = guiBackgroundColor;
+
+            EditorGUILayout.LabelField($"Objects Available: {objectPool.UnUsedObjectCount} / {objectPool.PoolSize}");
+        }
+
+        public override bool RequiresConstantRepaint()
+        {
+            return true;
+        }
+    }
+}
