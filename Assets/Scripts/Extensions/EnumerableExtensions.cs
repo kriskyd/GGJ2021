391ba65ed64b8beea2f712b5a453e3d73e7db663
kriskyd <dainarudd@gmail.com>
2021-01-29 22:47:42 +0100

    added extensions

---
+++ Assets/Scripts/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class EnumerableExtensions
+{
+	public static T Clone<T, V>(this T enumerable) where T : IEnumerable<V>, new() where V : IClonable<V>
+	{
+		if(enumerable == null)
+			return new T();
+
+		T copy = new T();
+		foreach(var item in enumerable)
+			if(item != null)
+				copy.Append(item.Clone());
+		return copy;
+	}
+
+	public static List<V> Clone<V>(this List<V> list) where V : IClonable<V>
+	{
+		if(list == null)
+			return new List<V>();
+
+		List<V> copy = new List<V>();
+		foreach(var item in list)
+			if(item != null)
+				copy.Add(item.Clone());
+		return copy;
+	}
+
+	public static int IndexOf<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+	{
+		int index = 0;
+		foreach(var element in collection)
+		{
+			if(predicate(element))
+				return index;
+			index++;
+		}
+
+		return -1;
+	}
+
+}
+
+public interface IClonable<T>
+{
+	T Clone();
+}
---
+++ Assets/Scripts/Extensions/Extensions.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System;
+using System.Reflection;
+using UnityEngine.UI;
+
+public static class Extensions
+{
+	public static void CopyTo<T>(this T copyFrom, ref Component copyTo) where T : Component
+	{
+		Type type = copyTo.GetType();
+		if(type != copyFrom.GetType()) return; // type mis-match
+		BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Default | BindingFlags.DeclaredOnly;
+		PropertyInfo[] pinfos = type.GetProperties(flags);
+		foreach(var pinfo in pinfos)
+		{
+			if(pinfo.CanWrite)
+			{
+				try
+				{
+					pinfo.SetValue(copyTo, pinfo.GetValue(copyFrom, null), null);
+				}
+				catch { } // In case of NotImplementedException being thrown. For some reason specifying that exception didn't seem to catch it, so I didn't catch anything specific.
+			}
+		}
+		FieldInfo[] finfos = type.GetFields(flags);
+		foreach(var finfo in finfos)
+		{
+			finfo.SetValue(copyTo, finfo.GetValue(copyFrom));
+		}
+		return;
+	}
+
+	public static void SetAutoNavigation(Selectable[] selectables, bool ignoreNotInteractable = true)
+	{
+		foreach(var v in selectables)
+		{
+			Navigation nav = v.navigation;
+			nav.mode = Navigation.Mode.Explicit;
+			v.navigation = nav;
+		}
+
+		foreach(var v in selectables)
+		{
+			Navigation nav = v.navigation;
+			nav.selectOnDown = FindSelectable(v, selectables, Vector3.down, ignoreNotInteractable);
+			nav.selectOnUp = FindSelectable(v, selectables, Vector3.up, ignoreNotInteractable);
+			nav.selectOnLeft = FindSelectable(v, selectables, Vector3.left, ignoreNotInteractable);
+			nav.selectOnRight = FindSelectable(v, selectables, Vector3.right, ignoreNotInteractable);
+			v.navigation = nav;
+		}
+	}
+
+	public static void SetNoNavigation(Selectable[] selectables)
+	{
+		foreach(var v in selectables)
+		{
+			Navigation nav = v.navigation;
+			nav.mode = Navigation.Mode.None;
+			v.navigation = nav;
+		}
+	}
+
+	public static Selectable FindSelectable(Selectable origin, Selectable[] selectables, Vector3 dir, bool ignoreNotInteractable = true)
+	{
+		dir = dir.normalized;
+		Vector3 localDir = Quaternion.Inverse(origin.transform.rotation) * dir;
+		Vector3 pos = origin.transform.TransformPoint(GetPointOnRectEdge(origin.transform as RectTransform, localDir));
+		float maxScore = Mathf.NegativeInfinity;
+		Selectable bestPick = null;
+		for(int i = 0; i < selectables.Length; ++i)
+		{
+			Selectable sel = selectables[i];
+
+			if(sel == origin || sel == null || !sel.gameObject.activeInHierarchy)
+				continue;
+
+			if((!sel.IsInteractable() && ignoreNotInteractable) || sel.navigation.mode == Navigation.Mode.None)
+				continue;
+
+			var selRect = sel.transform as RectTransform;
+			Vector3 selCenter = selRect != null ? (Vector3)selRect.rect.center : Vector3.zero;
+			Vector3 myVector = sel.transform.TransformPoint(selCenter) - pos;
+
+			float dot = Vector3.Dot(dir, myVector);
+
+			if(dot <= 0)
+				continue;
+
+			float score = dot / myVector.sqrMagnitude;
+
+			if(score > maxScore)
+			{
+				maxScore = score;
+				bestPick = sel;
+			}
+		}
+		return bestPick;
+	}
+
+	public static Vector3 GetPointOnRectEdge(RectTransform rect, Vector2 dir)
+	{
+		if(rect == null)
+			return Vector3.zero;
+		if(dir != Vector2.zero)
+			dir /= Mathf.Max(Mathf.Abs(dir.x), Mathf.Abs(dir.y));
+		dir = rect.rect.center + Vector2.Scale(rect.rect.size, dir * 0.5f);
+		return dir;
+	}
+}
