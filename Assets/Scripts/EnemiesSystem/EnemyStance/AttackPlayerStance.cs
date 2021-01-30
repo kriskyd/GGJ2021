df8fc2c4c5bf87b2cb50ab6e9eb8ea6d697a4e1b
Zielon <zielon94@gmail.com>
2021-01-30 11:34:59 +0100

    Added enemy attack state behaviour and setup animation controllers. Added new player prefab to sample scene.

--- Assets/Scripts/EnemiesSystem/EnemyStance/AttackPlayerStance.cs
+++ Assets/Scripts/EnemiesSystem/EnemyStance/AttackPlayerStance.cs
@@ -1,26 +1,44 @@
-﻿using SA.ScriptableData;
-using UnityEngine;
+﻿using UnityEngine;
+using SA.ScriptableData;
+using DG.Tweening;
 
 [CreateAssetMenu(fileName = "AttackPlayerStance", menuName = "Stances/AttackPlayer")]
 public class AttackPlayerStance : StanceSO
 {
-	[SerializeField]
-	private Vector3Value playerPosition;
+    [SerializeField]
+    private Vector3Value playerPosition;
+    [SerializeField]
+    private float attackDistance = 1.0f;
 
-	public override Stance Stance => Stance.AttackPlayer;
+    private const string attackStateBoolName = "AttackState";
+    private const string walkBoolName = "Walk";
+    private const string attackTriggerName = "Attack";
+    public override Stance Stance => Stance.AttackPlayer;
 
-	public override void InitializeStance(Enemy enemy)
-	{
-		//throw new System.NotImplementedException();
-	}
+    public override void InitializeStance(Enemy enemy)
+    {
+        enemy.Animator.SetBool(attackStateBoolName, true);
+    }
 
-	public override void PerformStance(Enemy enemy)
-	{
-		enemy.NavMeshAgent.SetDestination(playerPosition);
-	}
+    public override void PerformStance(Enemy enemy)
+    {
+        if (Vector3.Distance(playerPosition.Value, enemy.transform.position) <= attackDistance)
+        {
+            enemy.Animator.SetBool(walkBoolName, false);
+            enemy.transform.DOLookAt(playerPosition.Value, 0.1f, AxisConstraint.Y, Vector3.up);
+            enemy.NavMeshAgent.isStopped = true;
+            enemy.Animator.SetTrigger(attackTriggerName);
+        }
+        else
+        {
+            enemy.NavMeshAgent.Resume();
+            enemy.NavMeshAgent.SetDestination(playerPosition);
+            enemy.Animator.SetBool(walkBoolName, true);
+        }
+    }
 
-	public override void DeinitializeStance(Enemy enemy)
-	{
-		//throw new System.NotImplementedException();
-	}
+    public override void DeinitializeStance(Enemy enemy)
+    {
+        enemy.Animator.SetBool(attackStateBoolName, false);
+    }
 }
