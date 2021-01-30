36ec1dab5a53ea6267c6bb0efb69ddb17d009a6d
norbert_blaszczyk_bigcheese <norbert.blaszczyk@bigcheesestudio.com>
2021-01-30 11:09:59 +0100

    Added Temporary rocket, rocket parts and rocket slots

---
+++ Assets/Scripts/RocketSystem/RocketPartData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace RocketSystem
+{
+	[CreateAssetMenu(fileName = "RocketPartData", menuName = "RocketSystem/RocketParts/RocketPartData")]
+	public class RocketPartData : ScriptableObject
+	{
+		[SerializeField]
+		private Color outlineColor;
+
+		public Color OutlineColor => outlineColor;
+		public RocketPart RocketPart { get; set; }
+		public RocketPartSlot RocketPartSlot { get; set; }
+	}
+}
---
+++ Assets/Scripts/RocketSystem/RocketPartSlot.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace RocketSystem
+{
+	public class RocketPartSlot : MonoBehaviour
+	{
+		[SerializeField]
+		private RocketPartData rocketPartData;
+
+		public RocketPartData RocketPartData => rocketPartData;
+
+		private void Start()
+		{
+			rocketPartData.RocketPartSlot = this;
+		}
+
+		public void PlaceRocketPart(RocketPart rocketPart)
+		{
+			rocketPart.transform.SetParent(this.transform);
+			rocketPart.transform.localPosition = Vector3.zero;
+		}
+
+#if UNITY_EDITOR
+		[ExecuteInEditMode()]
+		private void OnDrawGizmos()
+		{
+			Gizmos.color = rocketPartData.OutlineColor;
+			Gizmos.DrawWireSphere(transform.position, 0.1f);
+		}
+#endif
+	}
+}
